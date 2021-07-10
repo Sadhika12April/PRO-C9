751e394a06facfc150062fd3e010c01d1e7daab9
Sadhika12April <86913950+Sadhika12April@users.noreply.github.com>
2021-07-10 20:20:18 +0530

    Add files via upload

---
+++ p5play_template-main (9)/p5play_template-main/sketch.js
@@ -0,0 +1,32 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() {
+  background("black");
+if(keyIsDown(DOWN_ARROW))
+{
+  background('yellow');
+
+}
+if(keyIsDown(UP_ARROW))
+{
+  background('red');
+
+}
+if(keyIsDown(LEFT_ARROW))
+{
+  background('green');
+
+}
+if(keyIsDown(RIGHT_ARROW))
+{
+  background('blue');
+
+}
+}
+
+
+
+
